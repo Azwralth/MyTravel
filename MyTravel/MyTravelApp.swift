1805f899a8f1c4b1c45c8c27da70567386c5c69c
Vladislav <mackva34@gmail.com>
2024-06-18 16:34:40 +0200

    Initial Commit

---
+++ MyTravel/MyTravelApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyTravelApp.swift
+//  MyTravel
+//
+//  Created by Владислав Соколов on 18.06.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyTravelApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
